dcc5aa4054a39437f0d822070ca15d13dc66d3f6
tasina <tasin-a@hotmail.com>
2017-11-24 08:52:11 +0100

    First init

---
+++ Propositions/Propositions/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Propositions.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Propositions")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Propositions")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("2df68958-4fba-4f6c-8c05-3f86f60b4cb0")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Propositions/Propositions/Program.fs
@@ -0,0 +1,26 @@
+﻿//
+//Compulsory	Assignment:	Propositions
+//Af Hardy, Rasmus og Tasin
+//
+
+// Evaluate
+
+// Verify
+
+// Tautology
+
+// Satisfiable
+
+// Equality
+
+// ToString
+
+// Names used
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
+
+   
