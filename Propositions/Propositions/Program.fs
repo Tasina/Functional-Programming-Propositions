0934b356eedb21223133dc892ba2497cef855f9b
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2017-11-30 19:59:58 +0100

    done!

--- Propositions/Propositions/Program.fs
+++ Propositions/Propositions/Program.fs
@@ -1,7 +1,9 @@
-﻿    // INSTANTIATE A MAP WITH MOCK DATA (of key-value pairs)
+﻿    printfn "Compulsory Assignment: Propositions\n\tby Rasmus, Tasin & Hardy\n\n"
+
+    // INSTANTIATE A MAP WITH MOCK DATA (of key-value pairs)
     let dataMap = Map.ofList ["a", true; "b", false; "c", true]
 
-    // DECLARE EXPRESSIONS WITH STRONGLY TYPED TYPES (like an interface in c#)
+    // DECLARE DISCRIMINATED UNIONS (like an interface in c# with strongly typed types)
     type Expression =
     | Value of bool
     | Not of Expression
@@ -9,7 +11,7 @@
     | Or of Expression * Expression
     | Equals of Expression * Expression
     | Implies of Expression * Expression
-    | ExculsiveOr of Expression * Expression
+    | ExclusiveOr of Expression * Expression
     | Variable of string
 
 
@@ -27,19 +29,19 @@
         | Or (x, y) -> Evaluate map x || Evaluate map y
         | Equals (x, y) -> Evaluate map x = Evaluate map y
         | Implies (x, y) -> (not (Evaluate map x ) || Evaluate map y)
-        | ExculsiveOr (x, y) -> Evaluate map x <> Evaluate map y
-        | Variable id -> map.[id]
+        | ExclusiveOr (x, y) -> Evaluate map x <> Evaluate map y
+        | Variable id -> map.[id]   
 
     // INSTANTIATE EVALUATION FUNCTIONS
     let NotEvaluate = Evaluate dataMap (Not(Variable "b"))
     let AndEvaluate = Evaluate dataMap (And(Variable "a", Variable "c"))
     let OrEvaluate = Evaluate dataMap (Or(Variable "a", Variable "b"))
-    let EqualsEvaluate = Evaluate dataMap (Equals(Variable "a", Value true))
+    let EqualsEvaluate = Evaluate dataMap (Equals(Variable "a", Variable "c"))
     let ImpliesEvaluate = Evaluate dataMap (Implies(Variable "b", Variable "b"))
-    let ExclusiveOrEvaluate = Evaluate dataMap (ExculsiveOr(Variable "a", Variable "b"))
+    let ExclusiveOrEvaluate = Evaluate dataMap (ExclusiveOr(Variable "a", Variable "b"))
     
     // EXECUTE EVALUATION FUNCTIONS
-    printfn "NotEvaluate -> %b" NotEvaluate
+    printfn "Evaluate exercise:\nNotEvaluate -> %b" NotEvaluate
     printfn "AndEvaluate -> %b" AndEvaluate
     printfn "OrEvaluate -> %b" OrEvaluate
     printfn "EqualsEvaluate -> %b" EqualsEvaluate
@@ -48,14 +50,68 @@
 
 
 
+    (* --- Verify --- *)
+
+
+
+    // INSTANTIATE RECURSIVELY MATCHING FUNCTION
+    let rec Verify (map:Map<string,bool>) exp =
+        match exp with
+        | Value n -> n
+        | Not (x) -> Verify map x
+        | And (x, y) -> Verify map x && Verify map y
+        | Or (x, y) -> Verify map x && Verify map y
+        | Equals (x, y) -> Verify map x && Verify map y
+        | Implies (x, y) -> Verify map x && Verify map y
+        | ExclusiveOr (x, y) -> Verify map x && Verify map y
+        | Variable id -> map.ContainsKey(id)
+
+    // INSTANTIATE VERIFY FUNCTIONS
+    let NotVerify = Verify dataMap (Not(Variable "a"))
+    let AndVerify = Verify dataMap (And(Variable "b", Variable "c"))
+    let OrVerify = Verify dataMap (Or(Variable "d", Variable "e"))
+    let EqualsVerify = Verify dataMap (Equals(Variable "f", Variable "g"))
+    let ImpliesVerify = Verify dataMap (Implies(Variable "h", Variable "i"))
+    let ExclusiveOrVerify = Verify dataMap (ExclusiveOr(Variable "j", Variable "k"))
+    
+    // EXECUTE VERIFY FUNCTIONS
+    printfn "Verify exercise:\nNotVerify -> %A" NotVerify
+    printfn "AndVerify -> %A" AndVerify
+    printfn "OrVerify -> %A" OrVerify
+    printfn "EqualsVerify -> %A" EqualsVerify
+    printfn "ImpliesVerify -> %A" ImpliesVerify
+    printfn "ExclusiveOrVerify -> %A \n" ExclusiveOrVerify
+
+
+
     (* --- Names used --- *)
     
 
 
-    let NamesUsed = dataMap |> Map.toList |> List.map (fun (fst, _) -> fst)
-    for name in NamesUsed do printfn "NamesUsed -> %A" name
-    printf("\n")
+    // INSTANTIATE RECURSIVELY MATCHING FUNCTION
+    let rec NamesUsed exp = 
+        match exp with
+        | Value n -> []
+        | Not (x) -> NamesUsed x
+        | And (x, y) -> List.append (NamesUsed x) (NamesUsed y)
+        | Or (x, y) -> List.append (NamesUsed x) (NamesUsed y)
+        | Equals (x, y) -> List.append (NamesUsed x) (NamesUsed y)
+        | Implies (x, y) -> List.append (NamesUsed x) (NamesUsed y)
+        | ExclusiveOr (x, y) -> List.append (NamesUsed x) (NamesUsed y)
+        | Variable id -> [id]
 
-
-
-   
+    // INSTANTIATE NAMESUSED FUNCTIONS
+    let NotNamesUsed = NamesUsed (Not(Variable "a"))
+    let AndNamesUsed = NamesUsed (And(Variable "b", Variable "c"))
+    let OrNamesUsed = NamesUsed (Or(Variable "d", Variable "e"))
+    let EqualsNamesUsed = NamesUsed (Equals(Variable "f", Variable "g"))
+    let ImpliesNamesUsed = NamesUsed (Implies(Variable "h", Variable "i"))
+    let ExclusiveOrNamesUsed = NamesUsed (ExclusiveOr(Variable "j", Variable "k"))
+    
+    // EXECUTE NAMESUSED FUNCTIONS
+    printfn "NamesUsed exercise:\nNotNamesUsed -> %A" NotNamesUsed
+    printfn "AndNamesUsed -> %A" AndNamesUsed
+    printfn "OrNamesUsed -> %A" OrNamesUsed
+    printfn "EqualsNamesUsed -> %A" EqualsNamesUsed
+    printfn "ImpliesNamesUsed -> %A" ImpliesNamesUsed
+    printfn "ExclusiveOrNamesUsed -> %A \n" ExclusiveOrNamesUsed
